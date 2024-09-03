eebfd90bd6adef1d48c33cedc7f89c4dd6049c15
Akkiweb-ctrl <rananawareakash001@gmail.com>
2024-09-03 20:06:30 +0530

    missing elements made responsive

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -46,7 +46,7 @@ const Experience = () => {
                     return (
                       <span
                         key={index}
-                        className="mr-2 mt-4 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                        className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
                       >
                         {tech}
                       </span>
